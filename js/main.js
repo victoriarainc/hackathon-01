a3806bf7a8832968347a7aadf8ae4552b46cd957
showeimer <showeimer@gmail.com>
2017-07-20 00:18:42 -0400

    initial commit, created unscramble game/unfinished

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let score = 0;
+
+function unscrambleGame() {
+
+  let unScrambleBox = document.querySelector('#unScrambleBox');
+  unScrambleBox.innerHTML = '';
+
+  let word = "";
+  let scrambledWord = "";
+
+  fetch('http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5')
+
+    .then(function(response) {
+      // console.log(response);
+      response.json().then(function(data) {
+          word = data[0].word.toLowerCase();
+          console.log(word);
+        })
+
+        .then(function() {
+          scrambledWord = word.split('').sort(function() {
+            return 0.9 - Math.random()
+          }).join('');
+          console.log(scrambledWord);
+          unScrambleBox.innerHTML = `
+            <h3>Score: ${score}</h3>
+            <h3>${scrambledWord}</h3>
+            <input type="text" name="" value="" id="unscrambleInput"><button type="button" name="button" id="unscrambleButton">Ok</button>
+            `;
+        })
+
+        .then(function() {
+          document.querySelector('#unscrambleButton').addEventListener('click', function() {
+            if (document.querySelector('#unscrambleInput').value === word) {
+              score++
+              unScrambleBox.innerHTML = `
+                <h1>Great Job!</h1>
+                <button onclick="unscrambleGame()" id="startButton">Next</button>
+                `
+            } else {
+              console.log('Try again');
+            }
+          })
+        })
+
+    })
+}
