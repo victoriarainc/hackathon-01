302c55e859ad5a6857c6157dcf362b897c2dc2ef
Victoria Rain Coleman <victoriarainc@gmail.com>
2017-07-20 20:54:18 -0400

    Included reset button and fixed bugs

--- js/cyoa.js
+++ js/cyoa.js
@@ -4,24 +4,114 @@
 //   'Cupboard': "Cupboard, eh? " + userName + ", it's okay I'm a Harry Potter fan too! And as you can see, we've got this cozy space under the stairs for students to use as a conference room. Jamie is over there in the corner coding away... Hey Jamie! Let's not bother him too much."
 // }
 
-let error = "I'm sorry, I didn't catch that.";
+// ====== New Section ===== //
 
+let name = "";
+let dialog = {
+  'Clock Tower': "As we climb to the second story we can sneak a peek into the Clocktower here - one of our classrooms. Looks like Brandon here is brainstorming some pseudo code on our whiteboard wall! The room is also equipped with a projector screen for live demos and sometimes entertainment purposes. We've got a bright and beautiful mural on that back wall and, obviously, our homage to Back to the Future framed next to the screen.",
+  'Cloud City': "A little warm up here I know but the students here have a great working space here in Cloud City. A projecter and plenty of white board space to collaborate. And clearly if you're in here you should join the Dark Side. We have cookies. Just ask Victoria here! Oh you do have some cookies? Oh. They're gluten free. No thanks.",
+  'Cupboard': "Cupboard, eh? I'm a Harry Potter fan too! And as you can see, we've got this cozy space under the stairs for students to use as a conference room. Jamie is over there in the corner coding away... Hey Jamie! Let's not bother him too much.",
+  'Lab': "Now that our class sizes have grown and we've integrated our program to include both front-end and back-end development for every student the Lab here has been the main hub of our location. At the end of the day students typically have lab time but they are not restricted to this area for it. Ben and Grant here seem to be working on their daily project so we'll just move right along. In the back of the room, as you can see, is a somewhat intimidating staircase that leads right up to our second story!",
+  'Kitchen': "The heart of our campus is really the kitchen here. Just look around! A beautiful mural to start the day off right, a real, vintage coffee pot, and even a hard-working microwave for all to share!"
+}
 let promptStrings = [
   'Where to next?',
   'Where would you like to go?',
   'Lets check out a room!',
   'What would you like to see?'
 ]
-// ================
-// ==== TOUR   ====
-// ================
+let error = "I'm sorry, I didn't catch that.";
 
 let rooms = ['Clock Tower', 'Cupboard', 'Cloud City', 'Lab', 'Kitchen'];
 let visitedRooms = [];
 
+document.getElementById('beginButton').addEventListener('click', displayNamePrompt);
+
+function displayNamePrompt() {
+  // Make sure the adventure is reset
+  name = '';
+  visitedRooms = [];
+  
+  let template = `
+  <p>This is a start message.  Edit ME!!!!!</p>
+  <input id="nameInput" placeholder="Name"></input>
+  <button id="nameButton">Submit</button>
+  `
+  document.getElementById('cyoaBox').innerHTML = template;
+  document.getElementById('nameButton').addEventListener('click', saveName);
+}
+
+function saveName() {
+  name = document.getElementById('nameInput').value;
+  displayGreeting();
+}
+
+function displayGreeting() {
+  let template = `
+  <p>Hi ${name}! I'm Stacey, I'll be your guide around the Iron Yard campus today.</p>
+  <p>Where would you like to go first?</p>
+  `
+  document.getElementById('cyoaBox').innerHTML = template;
+  displayRoomPrompt();
+}
+
+function displayRoomPrompt() {
+  let room_string = availableRooms().join(', ');
+  let template = `
+  <p>${room_string}</p>
+  <input id="roomInput"></input>
+  <button id="roomButton">Submit</button>
+  `
+  document.getElementById('cyoaBox').innerHTML += template;
+  document.getElementById('roomButton').addEventListener('click', displayNextRoom);
+}
+
+function displayNextRoom() {
+
+  // Figure out what the user entered
+  let selected_room = document.getElementById('roomInput').value;
+  console.log(selected_room);
+
+  if (availableRooms().indexOf(selected_room) === -1) {
+    document.getElementById('cyoaBox').innerHTML += `<p>${error}</p>`;
+    document.getElementById('roomButton').addEventListener('click', displayNextRoom);
+    return;
+  }
+
+  // User selected a valid room, so mark the new room as visited
+  visitedRooms.push(selected_room);
+
+  let template = `
+  <p>${dialog[selected_room]}</p>
+  `
+  document.getElementById('cyoaBox').innerHTML = template;
+
+  if (availableRooms().length > 0) {
+    let prompt_string = promptStrings[Math.floor(Math.random() * promptStrings.length)];
+    template = `
+    <p>${prompt_string}</p>
+    `
+    document.getElementById('cyoaBox').innerHTML += template;
+    displayRoomPrompt();
+
+  } else {
+      // No more rooms, display farewell
+      template = `
+      <p>Well that's The Iron Yard! It was really nice meeting you ${name}! I hope you liked our campus - we'll see you next time!</p>
+      <button id="resetCYOAButton">Restart</button>
+      `
+      document.getElementById('cyoaBox').innerHTML += template;
+      document.getElementById('resetCYOAButton').addEventListener('click', displayNamePrompt);
+  }
+}
+
+// ================
+// ==== TOUR   ====
+// ================
+
 // This function will calculate which rooms a user has already visited.
 // We then return an array of rooms we still need to visit.
-function availableRooms(rooms, visitedRooms) {
+function availableRooms() {
   let result = [];
   rooms.forEach(function(room) {
     if (visitedRooms.indexOf(room) === -1) {
@@ -35,7 +125,7 @@ function availableRooms(rooms, visitedRooms) {
 // all rooms the user has already visited.  If the user inputs an incorrect room
 // an error message will be displayed.  The selected room is then returned
 function promptForRoom() {
-  let room_string = availableRooms(rooms, visitedRooms).join(', ');
+  let room_string = availableRooms().join(', ');
   let prompt_string = promptStrings[Math.floor(Math.random() * promptStrings.length)];
 
   let answer = prompt(`${prompt_string} ${room_string}`);
@@ -50,11 +140,12 @@ function promptForRoom() {
 }
 
 // This will start the minigame
-cyoaBox.addEventListener('onclick', function cyoa() {
+// let cyoaBox = document.querySelector('#cyoaBox');
+// cyoaBox.addEventListener('click', cyoa);
 
 function cyoa() {
 
-      let cyoaBox = document.querySelector('#cyoaBox');
+      // let cyoaBox = document.querySelector('#cyoaBox');
 
       let userName = prompt("Who are you?");
       let firstQuestion = alert("Hi " + userName + "! I'm Stacey, I'll be your guide around the Iron Yard campus today.")
@@ -68,9 +159,7 @@ function cyoa() {
         'Lab': "Now that our class sizes have grown and we've integrated our program to include both front-end and back-end development for every student the Lab here has been the main hub of our location. At the end of the day students typically have lab time but they are not restricted to this area for it. Ben and Grant here seem to be working on their daily project so we'll just move right along. In the back of the room, as you can see, is a somewhat intimidating staircase that leads right up to our second story!",
         'Kitchen': "The heart of our campus is really the kitchen here. Just look around! A beautiful mural to start the day off right, a real, vintage coffee pot, and even a hard-working microwave for all to share!"
 
-        // cyoaBox.innerHTML = '';
       }
-    }
 
     // TODO: Print greeting message (without any room prompt)
 
@@ -89,10 +178,9 @@ function cyoa() {
 
     // Print the fairwell message
 
-    let exit = alert("Well that's The Iron Yard! It was really nice meeting you " + userName + "! I hope you liked our campus - we'll see you next time!")
+    let exit = alert("Well that's The Iron Yard! It was really nice meeting you " + userName + "! I hope you liked our campus - we'll see you next time!");
 
   }
-)
 
 
 // ===== Rough Draft ===== //
